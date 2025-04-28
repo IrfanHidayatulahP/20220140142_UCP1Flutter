0c63246ac39e55003119d301cada5f70e52c2566
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-04-29 00:53:31 +0700

    menambah file data_pelanggan

---
+++ lib/data_pelanggan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataPelanggan extends StatefulWidget {
+  const DataPelanggan({super.key});
+
+  @override
+  State<DataPelanggan> createState() => _DataPelangganState();
+}
+
+class _DataPelangganState extends State<DataPelanggan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
