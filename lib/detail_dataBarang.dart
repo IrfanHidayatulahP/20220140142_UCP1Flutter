1bef5534da61c9a495d4b3e6c49a79df118e8d4b
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-04-29 16:17:16 +0700

    membuat halaman DetailDataBarang

---
+++ lib/detail_dataBarang.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailDatabarang extends StatefulWidget {
+  const DetailDatabarang({super.key});
+
+  @override
+  State<DetailDatabarang> createState() => _DetailDatabarangState();
+}
+
+class _DetailDatabarangState extends State<DetailDatabarang> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
