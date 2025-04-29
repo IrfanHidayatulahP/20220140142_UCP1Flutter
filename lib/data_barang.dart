e4906e508f655166a53ea085a0f09335ff114c1a
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-04-29 14:15:36 +0700

    membuat file DataBarang

---
+++ lib/data_barang.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DataBarang extends StatefulWidget {
+  const DataBarang({super.key});
+
+  @override
+  State<DataBarang> createState() => _DataBarangState();
+}
+
+class _DataBarangState extends State<DataBarang> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
